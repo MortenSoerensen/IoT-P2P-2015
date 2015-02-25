5da424c4866e355b74c6c9e95025cb046505d492
metamate <jakikob@gmail.com>
2015-02-25 13:01:45 +0100

    fingerfun

--- FingerChord.ts
+++ FingerChord.ts
@@ -3,7 +3,7 @@
 class FingerChord extends Chord
 {
     static fingertableSize : number = 3;
-    static fingertableSpan : number = Math.pow(2, (FingerChord.fingertableSize-1));
+    static fingertableSpan : number = Math.pow(2, (FingerChord.fingertableSize));
     private fingertable : Array<NodeInfo>;
 
     constructor()
@@ -18,6 +18,62 @@ class FingerChord extends Chord
     {
         if(path_name === "/update_finger")
         {
+
+            function get_successor(port, callback)
+            {
+                // Setup the HTTP request
+                var options = {
+                    host: 'localhost',
+                    port: port,
+                    path: '/suc'
+                };
+                // ... and fire!
+                http.get(options, function(res)
+                {
+                    res.on("data", function(chunk)
+                    {
+                        callback(JSON.parse(chunk));
+                    });
+                }).on('error', function(e)
+                {
+                    console.error("Error while calling get_predecessor: " + e.message);
+                    console.error("Terminating");
+                    process.exit(1);
+                });
+            }
+            var _this = this;
+            var checkForTableExpansion = function recursive_suc(nodesVisited : number, current: NodeInfo, callback : () => void)
+            {
+               /* if(current.port === _this.info.port)
+                {
+                    console.log("Reached the current node, fingertable updated.");
+                    callback();
+                    return;
+                }*/
+                for(var i=1;i<=FingerChord.fingertableSize;i++)
+                {
+                    var l = Math.pow(2,i);
+                    if(nodesVisited == l)
+                    {
+                        _this.fingertable[i-1] = current;
+                    }
+                }
+                if(nodesVisited === FingerChord.fingertableSpan)
+                {
+                    console.log("Done updating fingertable.");
+                    callback();
+                    return;
+                }
+                else
+                {
+                    // Get the successor of current, and recurse on that
+                    get_successor(current.port, function(successor: NodeInfo)
+                    {
+                        //build initial table, check for port?
+                        recursive_suc(nodesVisited+1, successor, callback);
+                    });
+                }
+            }
             // NOTE: Should probably take an id of the inserted node as url argument
             // XXX: var insertID = query['insertID']
             function update_finger_worker(callback : () => void)
@@ -26,13 +82,28 @@ class FingerChord extends Chord
                 //
                 // XXX: Loop through the fingers, find the insertion spot,
                 //      i.e. between which fingers the new node was inserted.
+                //for(var i = 0; i <= fingertableSize; i++){
+
+                //    if(this.info.id > insertID) {
+                //        this.fingertable[i] = get_predecessor("port of inserted node", callback);
+                //    }
+
+                //} //is our fingertable full now?
+                //if yes, return;
+
+                //go to the current nodes last finger entry
+                //var lastfinger : NodeInfo = fingertable[fingertable.length-1].info;
+                checkForTableExpansion(0, _this.info, callback);
+                //call suc recursively until you meet your own node (if so, the table should not be expanded), if you reach
+                //a "fingertablespan" amount of suc iterations, the reached node is now the next fingertable entry and the fingertablesize has increased by one
+
+
                 // XXX: Update all the ones after the insertion spot with +1
                 //      i.e. go to the finger, and find it predecessor, it's the new finger.
                 // XXX: Check if our finger table is filled, if so return now.
                 // XXX: Check if there's enough additional nodes to extend our finger table.
                 //      i.e. by doing a linear search forward.
                 // XXX: Call callback when everything is done
-                callback();
             }
 
             res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -43,13 +114,13 @@ class FingerChord extends Chord
                 res.end();
             });
         }
-        if(path_name === "/pre")
+        else if(path_name === "/pre")
         {
             res.writeHead(200, {'Content-Type': 'application/JSON'});
             res.write(JSON.stringify(this.predecessor));
             res.end();
         }
-        if(path_name === "/suc")
+        else if(path_name === "/suc")
         {
             res.writeHead(200, {'Content-Type': 'application/JSON'});
             res.write(JSON.stringify(this.successor));
@@ -138,6 +209,7 @@ class FingerChord extends Chord
             starter(0, _this.predecessor);
             // TODO: We need to build our own initial finger table
             // XXX: Ask someone for theirs, and use that to construct ours
+            // is the finger table of the predecessor not
         });
     }
 }
